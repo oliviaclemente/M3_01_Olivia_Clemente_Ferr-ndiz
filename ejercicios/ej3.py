6eb13f460762015f60af139366eb7ab41c2422ed
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-11-04 10:25:26 +0000

    c

--- ejercicios/ej3.py
+++ ejercicios/ej3.py
@@ -4,3 +4,11 @@ class Producto:
     self.nombre= nombre
     self.precio=precio
     self.tipo=tipo
+  def mostrar(self):
+    print("Codigo:",self.codigo)
+    print("Nombre:",self.nombre)
+    print("Precio:",self.precio)
+    print("Tipo:",self.tipo)
+  def cons(self):
+    print("El producto se ha creado con éxito")
+  def__str__
