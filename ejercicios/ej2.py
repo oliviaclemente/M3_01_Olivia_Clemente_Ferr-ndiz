93a2d4315a08fb3cf27902a11bffd8a55206a992
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-11-04 19:32:10 +0000

    c

--- ejercicios/ej2.py
+++ ejercicios/ej2.py
@@ -1,2 +1,23 @@
-def __str__(self,nombre,nota): 
-  
+class Alumno:
+  def constructor(self, nombre, nota):
+    self.nombre= nombre
+    self.nota=nota
+  def mostrar(self):
+    print("Nombre:",self.nombre)
+    print("Nota:",self.nota)
+  def calificacion(self):
+      if self.nota<5:
+        print("El alumno tiene un suspenso")
+      else:
+        print("El alumno tiene un aprobado")
+
+    
+alumno1=Alumno()
+alumno1.constructor("Lau",9)
+alumno1.mostrar()
+alumno1.calificacion()
+
+alumno2=Alumno()
+alumno2.constructor("Juls",4)
+alumno2.mostrar()
+alumno2.calificacion()
