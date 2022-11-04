5dc9074099a33a8fec006223c9768ff40bd8cdd2
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-11-04 10:11:59 +0000

    cc

--- ejercicios/ej1.py
+++ ejercicios/ej1.py
@@ -1,13 +1,23 @@
 class Alumno:
-  def constructor(self, nombre):
+  def constructor(self, nombre, nota):
     self.nombre= nombre
+    self.nota=nota
   def mostrar(self):
     print("Nombre:",self.nombre)
+    print("Nota:",self.nota)
+  def calificacion(self):
+      if self.nota<5:
+        print("El alumno tiene un suspenso")
+      else:
+        print("El alumno tiene un aprobado")
+
     
 alumno1=Alumno()
-alumno1.constructor("Lau")
+alumno1.constructor("Lau",9)
 alumno1.mostrar()
+alumno1.calificacion()
 
 alumno2=Alumno()
-alumno2.constructor("Juls")
+alumno2.constructor("Juls",4)
 alumno2.mostrar()
+alumno2.calificacion()
